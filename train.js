2c1aea984191ee0c0f01e54de6ae09ab8d2de55e
frank <azic17092000@gmail.com>
2026-04-17 11:29:22 +0900

    feat: MIT TASK JavaScript

---
+++ train.js
@@ -0,0 +1,22 @@
+/* MITASK-A
+
+Savol: Shunday 2 parametrli function tuzing, 
+hamda birinchi parametrdagi letterni ikkinchi parametrdagi 
+sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+
+// Masalaning yechimi:
+
+function countLetter(letter, word) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+};
+
+const result = countLetter("i", "primitive");
+console.log("result:", result);
