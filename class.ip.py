5947eb78bb5168706ca25c94d135ece49b8752eb
frank <azic17092000@gmail.com>
2026-04-22 07:48:42 +0900

    feat: Inheritance concept

---
+++ class.ip.py
@@ -0,0 +1,99 @@
+''' CLASS deep diving
+    (1) ENCAPSULATION
+    (2) INHERITANCE <
+    (3) POLYMORPHISM <
+'''
+
+print("===== INHERITANCE =====")
+# PARENT > CHILD [only provides public & protected properties(state + method)]
+
+
+class Animal:  # Parent
+    # state
+    description = "This class is parent for animals"
+
+    # constructor
+    def __init__(self, voice):
+        # self.status = "animal is alive"
+        # self.__status = "animal is alive"
+        self._status = "animal is alive"
+        self.voice = voice
+
+    # method
+    def make_voice(self):
+        print(f"the animal can make voice: {self.voice}")
+
+
+class Dog(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def protect(self):
+        print("Yes, I can protect you!")
+
+
+class Cat(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def play(self):
+        pass
+
+
+class Fish(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def swim(self):
+        print("Yes, I can swim!")
+
+
+dog = Dog("Rex", "woof", True)
+cat = Cat("Tom", "meow", True)
+fish = Fish("Nemo", "ZzZ", False)
+
+
+dog.introduce()
+cat.introduce()
+fish.introduce()
+
+print("-----")
+dog.make_voice()
+fish.make_voice()
+
+print("-----")
+print(Animal.description)
+print(Dog.description)
+
+print(dog.voice, fish.voice)
+# print("dog.status:", dog.status)
+# print("cat.status:", cat.status)
+print("dog.status:", dog._status)
+print("cat.status:", cat._status)
